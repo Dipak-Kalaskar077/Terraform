525ebf488bddbd84e73c14d0a59d84ca193d3ff3
Dipak-Kalaskar077 <er.ddkalaskar@gmail.com>
2025-01-13 22:18:30 +0530

    Basic Network Architecture Using Terraform

--- Launch_Instance.tf
+++ Launch_Instance.tf
@@ -1,14 +1,16 @@
-provider "aws" {
-  region = "us-east-1"
-}
+# provider "aws" {
+#   region = "us-east-1"
+# }
 
-resource "aws_instance" "myserver" {
-  ami                    = "ami-0e2c8caa4b6378d8c"
-  key_name               = "vaibhav.key.pem"
-  instance_type          = "t2.micro"
+# resource "aws_instance" "myserver" {
+#   ami                    = "ami-0e2c8caa4b6378d8c"
+#   key_name               = "vaibhav.key.pem"
+#   instance_type          = "t2.micro"
   
-  tags = { 
-    Name        = "My Server"  # Instance name tag
-    Environment = "dev"
-  }
-}
+#   tags = { 
+#     Name        = "My Server"  # Instance name tag
+#     Environment = "dev"
+#   }
+# }
+
+

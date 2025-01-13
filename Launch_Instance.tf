0552cb030746cb5351701647759db010ab1626f8
Dipak-Kalaskar077 <er.ddkalaskar@gmail.com>
2025-01-13 10:24:34 +0530

    Launch Instance Install Tomcat using Terraform

--- Launch_Instance.tf
+++ Launch_Instance.tf
@@ -10,15 +10,25 @@ resource "aws_instance" "myserver" {
 
   ## User data to install Java and Tomcat
   user_data              = <<-EOF
-                          #!/bin/bash
-                          sudo yum update -y
-                          sudo yum install -y java-1.8.0-openjdk
-                          sudo yum install -y wget
-                          wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.41/bin/apache-tomcat-9.0.41.tar.gz
-                          tar -xzf apache-tomcat-9.0.41.tar.gz
-                          sudo mv apache-tomcat-9.0.41 /usr/local/tomcat9
-                          sudo chmod +x /usr/local/tomcat9/bin/*.sh
-                          sudo /usr/local/tomcat9/bin/startup.sh
+                         #!/bin/bash
+# Update package lists and upgrade installed packages
+sudo apt update -y
+sudo apt upgrade -y
+
+# Install Java 8 and wget
+sudo apt install -y openjdk-8-jdk wget
+
+# Download and extract Tomcat
+wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.41/bin/apache-tomcat-9.0.41.tar.gz
+tar -xzf apache-tomcat-9.0.41.tar.gz
+
+# Move Tomcat to /usr/local and set permissions
+sudo mv apache-tomcat-9.0.41 /usr/local/tomcat9
+sudo chmod +x /usr/local/tomcat9/bin/*.sh
+
+# Start Tomcat server
+sudo /usr/local/tomcat9/bin/startup.sh
+
                           EOF
   tags = { 
     Name        = "dipak-terraform-instance"  # Instance name tag

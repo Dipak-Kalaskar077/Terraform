18799af696e4560b764b28d004574454f28e71d3
Dipak-Kalaskar077 <er.ddkalaskar@gmail.com>
2025-01-13 10:30:29 +0530

    Launch Instance Install Tomcat using Terraform

--- Launch_Instance.tf
+++ Launch_Instance.tf
@@ -11,23 +11,23 @@ resource "aws_instance" "myserver" {
   ## User data to install Java and Tomcat
   user_data              = <<-EOF
                          #!/bin/bash
-# Update package lists and upgrade installed packages
-sudo apt update -y
-sudo apt upgrade -y
+                        # Update package lists and upgrade installed packages
+                        sudo apt update -y
+                        sudo apt upgrade -y
 
-# Install Java 8 and wget
-sudo apt install -y openjdk-8-jdk wget
+                        # Install Java 8 and wget
+                        sudo apt install -y openjdk-8-jdk wget
 
-# Download and extract Tomcat
-wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.41/bin/apache-tomcat-9.0.41.tar.gz
-tar -xzf apache-tomcat-9.0.41.tar.gz
+                        # Download and extract Tomcat
+                        wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.41/bin/apache-tomcat-9.0.41.tar.gz
+                        tar -xzf apache-tomcat-9.0.41.tar.gz
 
-# Move Tomcat to /usr/local and set permissions
-sudo mv apache-tomcat-9.0.41 /usr/local/tomcat9
-sudo chmod +x /usr/local/tomcat9/bin/*.sh
+                        # Move Tomcat to /usr/local and set permissions
+                        sudo mv apache-tomcat-9.0.41 /usr/local/tomcat9
+                        sudo chmod +x /usr/local/tomcat9/bin/*.sh
 
-# Start Tomcat server
-sudo /usr/local/tomcat9/bin/startup.sh
+                        # Start Tomcat server
+                        sudo /usr/local/tomcat9/bin/startup.sh
 
                           EOF
   tags = { 
